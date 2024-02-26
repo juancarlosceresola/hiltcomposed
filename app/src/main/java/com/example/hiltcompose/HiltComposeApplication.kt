def775717b2d0e21a3756d26987efe6891bdb9dc
Juan carlos Ceresola <jc.ceresola@tyris-software.com>
2024-02-26 19:11:12 +0100

    Initial commit

---
+++ app/src/main/java/com/example/hiltcompose/HiltComposeApplication.kt
@@ -0,0 +1,8 @@
+package com.example.hiltcompose
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class HiltComposeApplication: Application() {
+}
---
+++ app/src/main/java/com/example/hiltcompose/MainActivityViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.hiltcompose
+
+class MainActivityViewModel {
+}
